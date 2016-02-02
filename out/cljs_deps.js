ccc8dceb5138ae7a5b09b511b9c7843e6b79b3f5
Jon Spalding <jon.spalding@googlemail.com>
2016-02-02 18:48:07 +0000

    Remove out dir from git

--- out/cljs_deps.js
+++
@@ -1,5 +0,0 @@
-goog.addDependency("base.js", ['goog'], []);
-goog.addDependency("../cljs/core.js", ['cljs.core'], ['goog.string', 'goog.object', 'goog.string.StringBuffer', 'goog.array']);
-goog.addDependency("../CD2EE4D.js", ['cljs.nodejs'], ['cljs.core']);
-goog.addDependency("../lisp_paredit/core.js", ['lisp_paredit.core'], ['cljs.core', 'cljs.nodejs']);
-goog.addDependency("../BCE03FE.js", ['cljs.nodejscli'], ['cljs.core', 'cljs.nodejs']);
