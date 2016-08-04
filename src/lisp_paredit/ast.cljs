9157a564d8d34b0a6ac80c9aca38d164688404e7
Jon Spalding <jon.spalding@googlemail.com>
2016-08-04 17:27:09 +0100

    Disable caching

--- src/lisp_paredit/ast.cljs
+++ src/lisp_paredit/ast.cljs
@@ -1,6 +1,8 @@
 (ns lisp-paredit.ast
   (:require [paredit-js.core :as paredit]))
 
+; Caching proved slower than calculating ast each time
+
 (def ^:private asts (atom {}))
 (def ^:private hashcodes (atom {}))
 
@@ -18,20 +20,28 @@
           hash
           (recur hash (inc i)))))))
 
-(defn get-ast
+(defn get-ast-cached
   "Get the ast for the editor. Store in a map keyed by the file path
-   to avoid parsing the same source more than once. Store the hashcodes to determine
-   if the file content has changed."
+  to avoid parsing the same source more than once. Store the hashcodes to determine
+  if the file content has changed."
   [editor]
-  (let [src               (.getText editor)
-        path              (.getPath editor)
-        hashcode          (hashcode src)
-        existing-hashcode (get @hashcodes path)
-        existing-ast      (get @asts path)]
+  (let [start (.now js/Date)
+         src               (.getText editor)
+         path              (.getPath editor)
+         hashcode          (hashcode src)
+         existing-hashcode (get @hashcodes path)
+         existing-ast      (get @asts path)]
+    (println "get-ast" start)
     (if (and existing-ast
              (= existing-hashcode hashcode))
       existing-ast
       (let [ast (paredit/parse src)]
         (swap! asts #(assoc % path ast))
         (swap! hashcodes #(assoc % path hashcode))
+        (println "get-ast fin" (- (.now js/Date) start))
         ast))))
+
+(defn get-ast
+  [editor]
+  (let [src (.getText editor)]
+    (paredit/parse src)))
